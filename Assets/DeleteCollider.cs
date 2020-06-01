54bf1f4f2fe9083c3fe4af4123b02cdb002ff922
Razi <raz010@csiro.au>
2020-06-01 17:04:55 +1000

    first commit

---
+++ Assets/DeleteCollider.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DeleteCollider : MonoBehaviour { 
+
+    public GameObject sphere;
+    public int goal;
+    public TextMeshPro Title;
+    public TextMeshPro Description;
+    public AudioSource scrapSound;
+    public AudioSource successSound;
+
+
+    private int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter (Collider col)
+    {
+        Destroy(col.gameObject);
+        count++;
+        scrapSound.Play();
+
+
+        if (count == goal)
+        {
+            Title.text = "COMPLETE";
+            Description.text = "You may now exit the program";
+            successSound.Play();
+            sphere.gameObject.SetActive(true);
+        }
+
+    }
+}
